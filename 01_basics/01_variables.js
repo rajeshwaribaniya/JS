e067b1e91cf0b06ca802e54b695d56becd8cc309
Rajeshwari Baniya <70877568+Rajeshwari1-1@users.noreply.github.com>
2024-02-05 15:41:04 +0000

    .

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+console.log("Variables");
+const accountID = 101;
+// accountID = 2; // not allowed
+//console.log(accountID); 
+
+let accountEmail= "helloworld@gmail.com";
+var accountPassword = "986543";
+accountCity = "Bharatpur";
+
+accountEmail = "byebyeworld@gmail.com";
+accountPassword= "26435889644";
+accountCity="Kathmandu";
+
+console.table([accountID,accountEmail,accountPassword,accountCity]);
+
