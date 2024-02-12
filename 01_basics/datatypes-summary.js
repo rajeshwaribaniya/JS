8e1ce739d6bebb35e1831f2160dc9cb43840e566
Rajeshwari Baniya <70877568+Rajeshwari1-1@users.noreply.github.com>
2024-02-12 14:23:06 +0000

    datatypes

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,16 @@
+// Two data types : primitive and non - primitive/ reference
+// Primitive --> call by value
+// 7 types
+/*  1. String
+    2. Number
+    3. Boolean
+    4. Null (like temperature not zero , its empty)
+    5. Undefined --> variable is only declared
+    6. Symbol --> To make value unique
+    7. BigInt
+*/
+
+// Reference type (Non- primitive)
+
+// Array, Objects, Functions
+
