3f3fee0ccdedc259d44a37a8beb5cc98d0ec2e81
Rajeshwari Baniya <70877568+Rajeshwari1-1@users.noreply.github.com>
2024-02-21 10:01:21 +0000

    arrays

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,15 @@
+// array
+
+// ()     ----------> parenthesis
+// []    -----------> brackets
+// {}    ----------> braces
+
+const myArray = [ 0, 1, 2, 3, 4, 5] // simple array  0 is a element
+const myAnotherArray = [ 0, 1, 2, 3, 4, 5, true, "Rajeshwari"]   // It is still an array
+console.log(myAnotherArray[7]);
+
+// Array is Object. In a single variable like myAnotherArray, we can store multiple items.
+
+const mySisters = ["Aishwarya", "Sushmita"];
+
+const myArray2 = new Array(1, 2, 3, 4);
