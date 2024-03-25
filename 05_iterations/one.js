4e1674fa0a9d02c0f0569d1e0109d86514944337
Rajeshwari Baniya <70877568+Rajeshwari1-1@users.noreply.github.com>
2024-03-25 09:05:33 +0000

    for loop

---
+++ 05_iterations/one.js
@@ -0,0 +1,52 @@
+// for 
+
+// for (let i = 0; i <= 10 ; i++){
+//     const element = i;
+//     console.log(element);
+// }
+
+// for (let i = 0; i <= 10 ; i++){
+//     const element = i;
+//     if ( element == 5){
+//         console.log("The number is 5.");
+//     }
+//     console.log(element);
+// }
+
+
+// windows multi cursor -> ctrl + shift + L
+
+// for (let i = 1; i <= 10; i++){
+//     console.log(`Outer loop value :${i}`);
+//     for ( let j = 1; j<=10; j++)
+//     // console.log(`Inner loop value: ${j} and value of i : ${i}`);
+//     console.log( i + '*' + j + "=" + i*j);
+ 
+// }
+
+// let myArray = [ "flash", "batman", "superman"]
+
+// for ( let i = 0; i < myArray.length; i++){
+//     const element = myArray[i];
+//     console.log(element);
+// }
+
+// break and continue
+
+// for ( let i = 1; i<= 20; i++){
+//     if ( i == 5){
+//         console.log("DETECTED 5");
+//         break;
+//     }
+//     console.log(`Value of i is ${i}`);
+// }
+
+for ( let i = 1; i<= 20; i++){
+    if ( i == 5){
+        console.log("DETECTED 5");
+        continue;
+    }
+    console.log(`Value of i is ${i}`);
+}
+
+// continue --> give one excuse
