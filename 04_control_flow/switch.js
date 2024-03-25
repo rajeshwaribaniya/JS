101d085c4c2d912bcf40892cbad610817061a203
Rajeshwari Baniya <70877568+Rajeshwari1-1@users.noreply.github.com>
2024-03-25 01:52:23 +0000

    switch

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,64 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+// const month = 3;
+// switch(month){
+//     case 1:
+//         console.log("January");
+//         break;
+
+//     case 2:
+//         console.log("February");
+//         break;
+
+//     case 3:
+//         console.log("March");
+//         break;
+
+//     case 4:
+//         console.log("April");
+//         break;
+
+//     case 5:
+//         console.log("May");
+//         break;
+
+//     default:
+//         console.log("Default case match");
+//         break;
+// }
+
+
+const month = "Jan";
+switch(month){
+    case "Jan":
+        console.log("January");
+        break;
+
+    case "Feb":
+        console.log("February");
+        break;
+
+    case "Mar":
+        console.log("March");
+        break;
+
+    case "Apr":
+        console.log("April");
+        break;
+
+    case "May":
+        console.log("May");
+        break;
+
+    default:
+        console.log("Default case match");
+        break;
+}
+
