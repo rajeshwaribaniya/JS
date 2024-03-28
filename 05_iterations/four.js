f2dd86f500187492b2927f8f5389a4a41539bd22
Rajeshwari Baniya <70877568+Rajeshwari1-1@users.noreply.github.com>
2024-03-28 01:30:48 +0000

    for-in loop

---
+++ 05_iterations/four.js
@@ -0,0 +1,29 @@
+// for-in loop
+// for (const key in object) {
+ 
+// }
+
+// generally object ma loop launa for-in loop
+
+// const myObject = {
+//     js : "javascript",
+//     cpp : "C++",
+//     rb : "ruby",
+//     swift : "swift by apple"
+// }
+
+// for (const key in myObject) {
+//     console.log(`${key} shortcut is for ${myObject[key]}`);
+// }
+
+// const programming = [ "js", "python", "ruby", "java"]
+// for (const key in programming) {
+//     console.log(key);
+// }
+
+// arrows keys are 0, 1, ...
+
+const programming = [ "js", "python", "ruby", "java"]
+for (const key in programming) {
+    console.log(programming[key]);
+}
